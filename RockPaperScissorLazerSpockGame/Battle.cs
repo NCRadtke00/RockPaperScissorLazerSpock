3b77afdfb9991abcb9891b8d789bd32186b5e800
NCRadtke00 <raddnc@gmail.com>
2020-08-17 11:06:34 -0500

    Working on game flow, started game introduction

--- RockPaperScissorLazerSpockGame/Battle.cs
+++ RockPaperScissorLazerSpockGame/Battle.cs
@@ -8,5 +8,23 @@ namespace RockPaperScissorLazerSpockGame
 {
     class Battle
     {
+        public Battle()
+        {
+
+        }
+        public void GameIntro()
+        {
+            Console.WriteLine("-------------------------------------------------------------------------");
+            Console.WriteLine("- Welcome to the fastest hand slinging shootout this side of the patomac!");
+            Console.WriteLine("--------------How many users do we have playing one or two?--------------");
+            Console.WriteLine("-------------------------------------------------------------------------");
+            Console.WriteLine("------------------------ ( 1 ) One User ---------------------------------");
+            Console.WriteLine("-------------------------------------------------------------------------");
+            Console.WriteLine("------------------------------ ( 2 ) Two Users --------------------------");
+            Console.WriteLine("-------------------------------------------------------------------------");
+            Console.WriteLine("-------------------------------------------------------------------------");
+
+        }
+
     }
 }
