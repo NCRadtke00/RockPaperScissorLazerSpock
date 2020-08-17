6a747177d01936912cc95f25fe1761547e7ab760
NCRadtke00 <raddnc@gmail.com>
2020-08-17 12:05:09 -0500

    fixed Picking a gesture from list in user.cs

--- RockPaperScissorLazerSpockGame/User.cs
+++ RockPaperScissorLazerSpockGame/User.cs
@@ -17,7 +17,32 @@ namespace RockPaperScissorLazerSpockGame
             Console.WriteLine("Pick the Gesture you would like to use! \n" + "--- 1 = Rock --- \n" + "--- 2 = paper --- \n" + "--- 3 = scissors --- \n" + "--- 4 = lizard --- \n" + "--- 5 = spock --- \n");
             string results = Console.ReadLine();
 
-            // if or switch statement
+            if (results == "1" || results == "One" || results == "one")
+            {
+                gesture = "rock";
+            }
+            else if (results == "2" || results == "Two" || results == "two")
+            {
+                gesture = "paper";
+            }
+            else if (results == "3" || results == "Three" || results == "three")
+            {
+                gesture = "scissors";
+            }
+            else if (results == "4" || results == "Four" || results == "four")
+            {
+                gesture = "lizard";
+            }
+            else if (results == "5" || results == "Five" || results == "five")
+            {
+                gesture = "spock";
+            }
+            else
+            {
+                Console.WriteLine("Please pick a gesture use its assigned number");
+            }
+
+
         }
 
     }
