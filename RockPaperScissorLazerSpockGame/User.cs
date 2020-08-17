dd682adf39e0ab6663e2e57eb3e698d62a3d225e
NCRadtke00 <raddnc@gmail.com>
2020-08-17 15:39:42 -0500

    fixing user.cs pickAGesture method

--- RockPaperScissorLazerSpockGame/User.cs
+++ RockPaperScissorLazerSpockGame/User.cs
@@ -14,8 +14,58 @@ namespace RockPaperScissorLazerSpockGame
         }
         public override string PickAGesture()
         {
-            Console.WriteLine("Please pick the hand gesture you would like to use! \n" + "--- 1 = Rock --- \n" + "--- 2 = paper --- \n" + "--- 3 = scissors --- \n" + "--- 4 = lizard --- \n" + "--- 5 = spock --- \n");
-            string results = Console.ReadLine();
+           
+            while (true)
+            {
+                Console.WriteLine("Please pick the hand gesture you would like to use! \n" + "--- 1 = Rock --- \n" + "--- 2 = paper --- \n" + "--- 3 = scissors --- \n" + "--- 4 = lizard --- \n" + "--- 5 = spock --- \n");
+                string results = Console.ReadLine();
+                if (results == "1" || results == "One" || results == "one")
+                {
+                    gesture = "rock";
+                    Console.WriteLine("You picked rock!");
+                    Console.ReadLine();
+                    return "rock";
+                }
+                else if (results == "2" || results == "Two" || results == "two")
+                {
+                    gesture = "paper";
+                    Console.WriteLine("You picked paper!");
+                    Console.ReadLine();
+
+                    return "paper";
+                }
+                else if (results == "3" || results == "Three" || results == "three")
+                {
+                    gesture = "scissors";
+                    Console.WriteLine("You picked scissors!");
+                    Console.ReadLine();
+
+                    return "scissors";
+                }
+                else if (results == "4" || results == "Four" || results == "four")
+                {
+                    gesture = "lizard";
+                    Console.WriteLine("You picked the lizard!");
+                    Console.ReadLine();
+
+                    return "lizard";
+
+                }
+                else if (results == "5" || results == "Five" || results == "five")
+                {
+                    gesture = "spock";
+                    Console.WriteLine("You picked the spock!");
+                    Console.ReadLine();
+                    return "spock";
+
+                }
+                else
+                {
+                    Console.WriteLine("Please try again and pick a gesture using its assigned number");
+                   
+                }
+            }
+           
             //ConsoleKeyInfo thing = Console.ReadKey();
 
             //if(thing.Key == ConsoleKey.D1)
@@ -27,52 +77,7 @@ namespace RockPaperScissorLazerSpockGame
             //}
             //else if (thing.Key == ConsoleKey.D2)
 
-            if (results == "1" || results == "One" || results == "one")
-            {
-                gesture = "rock";
-                Console.WriteLine("You picked rock!");
-                Console.ReadLine();
-                return "rock";
-            }
-            else if (results == "2" || results == "Two" || results == "two")
-            {
-                gesture = "paper";
-                Console.WriteLine("You picked paper!");
-                Console.ReadLine();
 
-                return "paper";
-            }
-            else if (results == "3" || results == "Three" || results == "three")
-            {
-                gesture = "scissors";
-                Console.WriteLine("You picked scissors!");
-                Console.ReadLine();
-
-                return "scissors";
-            }
-            else if (results == "4" || results == "Four" || results == "four")
-            {
-                gesture = "lizard";
-                Console.WriteLine("You picked the lizard!");
-                Console.ReadLine();
-
-                return "lizard";
-
-            }
-            else if (results == "5" || results == "Five" || results == "five")
-            {
-                gesture = "spock";
-                Console.WriteLine("You picked the spock!");
-                Console.ReadLine();
-                return "spock";
-
-            }
-            else
-            {
-                Console.WriteLine("Please try again and pick a gesture using its assigned number");
-                return results;
-               
-            }
             //switch (results)
             //{
             //    case "1":
